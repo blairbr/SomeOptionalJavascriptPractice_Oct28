82a4c1a292377de4be0e5f4c595f2f445f107982
Blair Brown <blair.j.brown@gmail.com>
2020-10-28 22:09:25 -0400

    adding files

---
+++ scripts.js
@@ -0,0 +1,12 @@
+console.log("hello");
+alert("Alert! Your javascript is working! You're a genius!")
+
+
+//Exercise 2
+//  let if = 0;  
+// console.log(if);
+
+//Exercise 3 - Template Strings
+// const name = "Adam";
+// let age = 12;
+// console.log(`Hello, my name is ${name}. I am ${age}.`); // "Hello, my name is Adam. I am 12."
